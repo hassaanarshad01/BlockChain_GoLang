fe0b5daddcacd87da76a28c8841570f253cdfc22
haiderhk <haiderkhan.1050@gmail.com>
2025-01-10 11:14:49 +0500

    Adjusted with the new hash function.

--- blockchain/block.go
+++ blockchain/block.go
@@ -9,16 +9,17 @@ import (
 
 // ========================Represents a transaction in the blockchain========================
 type Transaction struct {
-	DataHash string // Hash of the dataset stored on IPFS
-	AlgoHash string // Hash of the AI algorithm stored on IPFS
-	Output   string // Expected output of the algorithm
+	DataHash     string // CID of the dataset stored on IPFS
+	AlgoHash     string // CID of the AI algorithm stored on IPFS
+	Requirements string // CID of the requirements file stored on IPFS
+	Output       string // Hash of expected output of the algorithm
 }
 
 // ========================Represents a block in the blockchain========================
 type Block struct {
-	Hash         []byte        // Hash of the block
+	Hash         string        // Hash of the block
 	Transactions []Transaction // Transactions stored in the block
-	PrevHash     []byte        // Hash of the previous block
+	PrevHash     string        // Hash of the previous block
 	Nonce        int           // Nonce used in proof-of-work
 }
 
@@ -33,35 +34,37 @@ func (b *Block) GetHash() {
 	txData, _ := json.Marshal(b.Transactions)
 
 	// Join transaction data and previous hash
-	info := bytes.Join([][]byte{txData, b.PrevHash}, []byte{})
+	info := bytes.Join([][]byte{txData, []byte(b.PrevHash)}, []byte{})
 
 	// Compute the SHA-256 hash of the joined data
 	hash := sha256.Sum256(info)
 
 	// Store the hash in the block
-	b.Hash = hash[:]
+	b.Hash = hex.EncodeToString(hash[:])
 }
 
 // ========================Creates a new block========================
-func NewBlock(transactions []Transaction, prevhash []byte) *Block {
-	block := &Block{[]byte{}, transactions, prevhash, 0}
+func NewBlock(transactions []Transaction, prevhash string) *Block {
+	block := &Block{"", transactions, prevhash, 0}
 	pow := NewProof(block)
-	nonce, hash := pow.getHash()
+	nonce, hash := pow.GetHash()
 
-	block.Hash = hash[:]
+	block.Hash = hash
 	block.Nonce = nonce
 
 	return block
 }
 
 // ========================Initializes the blockchain with a genesis block========================
-func InitBlockchain() *Blockchain {
+func InitBlockchain() (*Blockchain, string) {
 	// Transaction stored in the Genesis Block
 	genesisTx := []Transaction{
-		{DataHash: "GenesisDataHash", AlgoHash: "GenesisAlgoHash", Output: "GenesisOutput"},
+		{DataHash: "GenesisData", AlgoHash: "GenesisAlgo", Requirements: "GenesisReq", Output: "GenesisOutputHash"},
 	}
 
-	return &Blockchain{[]*Block{NewBlock(genesisTx, []byte{})}}
+	block := NewBlock(genesisTx, "")
+
+	return &Blockchain{[]*Block{block}}, block.Hash
 }
 
 // ========================Adds a new block to the blockchain========================
@@ -79,15 +82,6 @@ func (chain *Blockchain) GetLatestBlock() *Block {
 // ========================Add a block to the chain========================
 func (chain *Blockchain) AddBlockToChain(block *Block) {
 	chain.Blocks = append(chain.Blocks, block)
-}
-
-// ========================Simple deterministic algorithm========================
-func DeterministicAlgorithm(data string) string {
-	var result byte
-	for _, b := range []byte(data) {
-		result ^= b
-	}
-	return hex.EncodeToString([]byte{result})
 }
 
 // ========================Hash data using SHA-256========================
